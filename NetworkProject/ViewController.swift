9a7d62d7b8d74af3c40a50b3dcca6749e82bc7d8
Maekiee <pdwssf@gmail.com>
2025-07-23 15:53:30 +0900

    Initial Commit

---
+++ NetworkProject/ViewController.swift
@@ -0,0 +1,53 @@
+import UIKit
+import SnapKit
+
+
+class ViewController: UIViewController {
+    
+    let goLottobutton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.title = "Go Lotto"
+        config.baseBackgroundColor = .orange
+        config.baseForegroundColor = .white
+        button.configuration = config
+        return button
+    }()
+    
+    let goMovieButton: UIButton = {
+        let button = UIButton()
+        var config = UIButton.Configuration.filled()
+        config.title = "Go Movie"
+        config.baseBackgroundColor = .black
+        config.baseForegroundColor = .white
+        button.configuration = config
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configuUI()
+        configLayout()
+    }
+    
+    func configuUI() {
+        view.addSubview(goLottobutton)
+        view.addSubview(goMovieButton)
+    }
+    
+    func configLayout() {
+        goLottobutton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
+            make.height.equalTo(44)
+        }
+        
+        goMovieButton.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+            make.top.equalTo(goLottobutton.snp.bottom).offset(20)
+            make.height.equalTo(44)
+            
+        }
+    }
+}
+
